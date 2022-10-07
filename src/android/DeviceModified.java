a3ef37ec7db3cf3dd05eb851568d887e0e00e84c
genusifabula <108719001+genusifabula@users.noreply.github.com>
2022-10-07 12:07:17 +0700

    Update DeviceModified.java

--- src/android/DeviceModified.java
+++ src/android/DeviceModified.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.devicemodified;
+package org.apache.cordova.device;
 
 import java.util.TimeZone;
 
